d4dd6166cd47f1da2d9c5ff243dab6780538da02
AmazyJoe <ochiengolu@gmail.com>
2022-11-22 08:38:02 +0300

    J.S lab2

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
-// Write your code in this file!
+const currentUser="Joseph Oluoch";
+const welcomeMessage=`Welcome to Flatbook, ${currentUser}!`;
+const excitedWelcomeMessage="WELCOME TO FLATBOOK, ${JOSEPH OLUOCH}!";
+const shortGreeting='Welcome, ${J}!'
